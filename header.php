3f78aed6110837010bfb4cd9927e360b5800b884
Sumitrc <119798250+Sumitrc@users.noreply.github.com>
2024-02-18 19:39:07 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,64 @@
+<!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Covid.</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" >Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reports</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Center
+          </a>
+          <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Symptoms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>     
+    </div>
+  </div>
+</nav>-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:red;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#" style="color:black;"><b>Covid.</b></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent " style="display: flex!important;
+    flex-basis: auto;
+    flex-direction: row-reverse;
+    align-items: center;">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#" style="color: D9D9D9;">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/science-in-5/episode-14---covid-19---tests?gclid=Cj0KCQjw0bunBhD9ARIsAAZl0E1vBgm9Ym_TUul1GA-273cVuuzJGxdUYlaUXVlwkhFBRKpzpqykll4aAlf6EALw_wcB" style="color: D9D9D9;">center</a>
+                </li>  <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: D9D9D9;">Reports</a>
+                   </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: D9D9D9;">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true" style="color: D9D9D9;">Symptoms</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: D9D9D9;">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
